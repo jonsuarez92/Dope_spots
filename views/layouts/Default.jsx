4fb08e873c0e2d6ad105f3c8e2f515b49173c0a3
Jonathan suarez <jonathansuarez@Jonathans-MBP.lan>
2022-03-15 17:31:28 -0400

    fix css issue

--- views/layouts/Default.jsx
+++ views/layouts/Default.jsx
@@ -12,9 +12,10 @@ class DefaultLayout extends React.Component {
                     <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
                     <title>My  Website to find dope spots</title>
-                    <link rel="stylesheet" href="/css/app.css"></link>
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"></link>
+                    <link rel="stylesheet" href="/css/app.css"></link>
                     </head>
+
         <body>
       <header>
      <h1> Dope Spots  </h1>
